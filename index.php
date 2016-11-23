c95567f90c6fb2594a3324687f218c93dccf197b
kaspermwh <kaspermwh@hotmail.com>
2016-11-23 14:00:30 +0100

    Initial Markup skew

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Aarhus Kickoff</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+    <main class="content col-lg-12">
+        
+        <section class="rightskew row">
+            <article class="info box col-lg-5">
+                <h2 class="box-header">Hej</h2>
+                <p class="box-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ante sed nulla ullamcorper hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dapibus dictum consequat. </p>
+            </article>
+            <article class="countdown box col-lg-7">
+                <h2 class="box-header">Hej</h2>
+                <p class="box-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ante sed nulla ullamcorper hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dapibus dictum consequat. orem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ante sed nulla ullamcorper hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dapibus dictum consequat.</p>
+            </article>
+        </section>
+        
+        <section class="leftskew row">
+            <article class="program box col-lg-7">
+                <h2 class="box-header">Hej</h2>
+                <p class="box-paragraph">Lorem ipsum henning er flot</p>
+            </article>
+            <article class="mad box col-lg-5">
+                <h2 class="box-header">Hej</h2>
+                <p class="box-paragraph">Lorem ipsum henning er flot</p>
+            </article>
+        </section>
+        
+        <section class="rightskew row">
+            <article class="partnere box col-lg-5">
+                <h2 class="box-header">Hej</h2>
+                <p class="box-paragraph">Lorem ipsum henning er flot</p>
+            </article>
+            <article class="talent box col-lg-7">
+                <h2 class="box-header">Hej</h2>
+                <p class="box-paragraph">Lorem ipsum henning er flot</p>
+            </article>
+        </section>
+        
+    </main>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
