aaa2ac381fc585e8bd51df0897df6e52e5dba119
kaspermwh <kaspermwh@hotmail.com>
2016-11-28 18:29:54 +0100

    Navbar created. Video working (still fixed).

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
 var jQuery = jQuery.noConflict(); 
-$(document).ready(function() {
+jQuery(document).ready(function() {
     console.log( "ready!" );
     
-    $('#indexwrapper').tubular({videoId: 'aV6ri6T406o'});
+    jQuery('#videowrapper').tubular({videoId: 'ua18_AEf77Q'});
     
 });
-
-/*$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 60) {
-                    $("#menu").css('background-color', 'blue');
-                } else {
-                    $("#menu").css('background-color', 'red');
-                }
-            });
-        });*/
-
-/** Super Simple Slider by @intllgnt **/
-
