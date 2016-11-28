4bcc20afe880718cf802e884ea94811cbf872a40
kaspermwh <kaspermwh@hotmail.com>
2016-11-29 00:07:58 +0100

    Added countdown

--- js/script.js
+++ js/script.js
@@ -5,3 +5,51 @@ jQuery(document).ready(function() {
     jQuery('#videowrapper').tubular({videoId: 'ua18_AEf77Q'});
     
 });
+
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.now();
+    //var hundreds = Math.floor((t % 1000 / 10));
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+    //'hundreds': hundreds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  //var hundredsSpan = clock.querySelector('.hundreds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    //hundredsSpan.innerHTML = ('' + t.hundreds).slice(-7);
+      
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date("2017-08-18T18:00:00");
+initializeClock('clockdiv', deadline);
