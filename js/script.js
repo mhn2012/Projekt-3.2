15e4b00a97c40494ee974278443bf2f298350273
kaspermwh <kaspermwh@hotmail.com>
2016-11-28 14:47:28 +0100

    Navbar v1. No video function. Whitespace after footer issue

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var jQuery = jQuery.noConflict(); 
+$(document).ready(function() {
+    console.log( "ready!" );
+    
+    $('#indexwrapper').tubular({videoId: 'aV6ri6T406o'});
+    
+});
+
+/*$(document).ready(function(){       
+            var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 60) {
+                    $("#menu").css('background-color', 'blue');
+                } else {
+                    $("#menu").css('background-color', 'red');
+                }
+            });
+        });*/
+
+/** Super Simple Slider by @intllgnt **/
+
